aa66f09a36ceb064205782ad93845e11f4a19862
IshaanSurya <84295173+IshaanSurya@users.noreply.github.com>
2021-05-19 19:54:25 -0400

    Add files via upload

---
+++ VivekaKendra/kannada/Ish_swara2.js
@@ -0,0 +1,38 @@
+<!--
+var numQues = 18;
+var answers = [];
+var sel = [];
+var attempts =0;
+
+
+var nRows = 10;
+var nCols= 4;
+
+var i_x=0;
+answers[i_x++] = "9";
+answers[i_x++] = "4";
+answers[i_x++] = "3";
+answers[i_x++] = "8";
+
+answers[i_x++] = "4";
+answers[i_x++] = "6";
+answers[i_x++] = "10";
+answers[i_x++] = "2";
+answers[i_x++] = "8";
+
+answers[i_x++] = "3";
+answers[i_x++] = "4";
+answers[i_x++] = "5";
+answers[i_x++] = "2";
+answers[i_x++] = "8";
+
+answers[i_x++] = "6";
+answers[i_x++] = "3";
+answers[i_x++] = "5";
+answers[i_x++] = "8";
+
+answers[i_x++] = "4";
+answers[i_x++] = "5";
+answers[i_x++] = "6";
+answers[i_x++] = "1";
+-->
