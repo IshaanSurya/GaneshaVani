c4b933f90412b5ce62da18d68002b747ae625638
IshaanSurya <84295173+IshaanSurya@users.noreply.github.com>
2021-11-20 13:32:27 -0500

    Add files via upload

---
+++ VivekaKendra/sheetPref.js
@@ -0,0 +1,9 @@
+var g_InstOrgName="Vivekananda Kendra Vidyalaya, Kallubalu." ;
+var g_dynSheetMoodImagePath="../../../images2/";
+var g_renderingType=""; //L (legacy), T (templated), Generated (G)
+var gv_FilterAns="GV_FA"
+
+// Search and replace string in files.
+// Note: (here [ is used as a boundery token. Any char after 's is considered as a boundary token)) sed -i 's[searchstring[replacestring[g
+// Actual example (here ` is the boundary token) : sed -i 's`</tbody>`<div id="di_GVL"> <span id="Sp_GVL"></span></div></tbody>`/g' *.html
+
