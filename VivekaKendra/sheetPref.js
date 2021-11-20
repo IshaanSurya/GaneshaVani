a9f2a8c53c1fb618fee298a203a229761e400a67
IshaanSurya <84295173+IshaanSurya@users.noreply.github.com>
2021-11-20 12:32:17 -0500

    Add files via upload

--- VivekaKendra/sheetPref.js
+++ VivekaKendra/sheetPref.js
@@ -1,6 +1,7 @@
 var g_InstOrgName="Vivekananda Kendra Vidyalaya, Kallubalu." ;
 var g_dynSheetMoodImagePath="../../../images2/";
 var g_renderingType=""; //L (legacy), T (templated), Generated (G)
+var gv_FilterAns="GV_FA"
 
 // Search and replace string in files.
 // Note: (here [ is used as a boundery token. Any char after 's is considered as a boundary token)) sed -i 's[searchstring[replacestring[g
